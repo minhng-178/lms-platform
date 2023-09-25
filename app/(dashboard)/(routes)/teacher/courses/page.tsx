10950487adf1461c8cd151fd1479354a9fadd541
minhng-178 <anhminh1782002@gmail.com>
2023-09-25 18:05:17 +0700

    prisma db setup

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,5 +1,14 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+
 const CoursesPage = () => {
-  return <div>Courses Page</div>;
+  return (
+    <div>
+      <Link href="/teacher/create">
+        <Button>Create Course</Button>
+      </Link>
+    </div>
+  );
 };
 
 export default CoursesPage;
